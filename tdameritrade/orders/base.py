6318d7755ade24194251536cb4b5fcd5b5421421
Jonathan Reyes <holostriker@gmail.com>
2020-05-24 23:41:11 -0400

    Add link to dataclass documentation

--- tdameritrade/orders/base.py
+++ tdameritrade/orders/base.py
@@ -4,5 +4,9 @@ from dataclasses import dataclass
 
 @dataclass
 class BaseOrder:
+    """Base Order dataclass
+
+    https://docs.python.org/3/library/dataclasses.html
+    """
     def json(self):
         return json.dumps(self.__dict__)
