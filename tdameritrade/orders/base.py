031c4c9654abefc2f87eb2b8aeb231f61d66d0b0
Jonathan Reyes <holostriker@gmail.com>
2020-05-24 21:07:16 -0400

    Change BaseOrder class into a dataclass

--- tdameritrade/orders/base.py
+++ tdameritrade/orders/base.py
@@ -1,20 +1,8 @@
 import json
+from dataclasses import dataclass
 
 
-class OrderEncoder(json.JSONEncoder):
-    def default(self, o):
-        return o.kwargs
-
-
+@dataclass
 class BaseOrder:
-    def __init__(self, **kwargs):
-        self.kwargs = kwargs
-    
     def json(self):
-        return json.dumps(self.kwargs, cls=OrderEncoder)
-
-    def __repr__(self):
-        return f"<{self.__class__.__name__}: {str(self)}>"
-
-    def __str__(self):
-        return str(self.kwargs)
+        return json.dumps(self.__dict__)
