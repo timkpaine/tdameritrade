3db557ef2c2ff2668ed10637b740d677d3669119
Jonathan Reyes <holostriker@gmail.com>
2020-06-12 21:42:32 -0400

    Add tests for orders submodule

--- tdameritrade/tests/unit/orders/test_orders.py
+++ tdameritrade/tests/unit/orders/test_orders.py
@@ -7,4 +7,20 @@ def test_build_buy_market_stock_order():
     symbol = "AAPL"
     quantity = 123
     order = orders.build_buy_market_stock_order(symbol, quantity)
-    assert order.json() == ""
+    json_str = order.json()
+    actual_order_dict = json.loads(json_str)
+    expected_order_dict = {
+        "duration": "DAY",
+        "orderLegCollection": [
+            {
+                "instruction": "BUY",
+                "instrument": {"assetType": "EQUITY", "symbol": symbol},
+                "quantity": quantity,
+            }
+        ],
+        "orderStrategyType": "SINGLE",
+        "orderType": "MARKET",
+        "session": "NORMAL",
+    }
+
+    assert expected_order_dict == actual_order_dict
