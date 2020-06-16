2484cb200813e5d65026ea3fca81e692539f3616
Jonathan Reyes <holostriker@gmail.com>
2020-06-15 23:11:00 -0400

    Expand order sample functions

--- tdameritrade/orders/orders.py
+++ tdameritrade/orders/orders.py
@@ -1,7 +1,7 @@
 from .base import BaseOrder
 from dataclasses import dataclass, field
 from typing import List, Any
-from .leg import OrderLeg, create_equity_order_leg
+from .leg import OrderLeg, create_equity_order_leg, create_option_order_leg
 from .activities import OrderActivity
 from .constants import (
     Instruction,
@@ -35,6 +35,13 @@ class CancelTime(BaseOrder):
 
 @dataclass
 class Order(BaseOrder):
+    """
+    Notes:
+        The int fields may not be actual ints even though the docs use an int as the default.
+        When I checked the sample orders, they made price into a string e.g., "6.45".
+
+        I made price a str for now.
+    """
     session: Session = None
     duration: Duration = None
     orderType: OrderType = None
@@ -53,7 +60,7 @@ class Order(BaseOrder):
     stopType: StopType = None
     priceLinkBasis: PriceLinkBasis = None
     priceLinkType: PriceLinkType = None
-    price: int = None
+    price: str = None
     taxLotMethod: TaxLotMethod = None
     orderLegCollection: List[OrderLeg] = None
     activationPrice: int = None
@@ -74,11 +81,12 @@ class Order(BaseOrder):
 
 def build_buy_market_stock_order(symbol, quantity):
     """Build Buy Market Stock Order
+
+    Buy {quantity} shares of {smybol} at the Market good for the Day.
     
     Simple sample from https://developer.tdameritrade.com/content/place-order-samples
 
     Args:
-        instruction: Buy or Sell
         symbol: symbol you want to trade
         quantity: How much of the stock to trade
     """
@@ -89,8 +97,6 @@ def build_buy_market_stock_order(symbol, quantity):
     order_strategy_type = OrderStrategyType.SINGLE
     # Can be changed to handle Buy and Sell for Equities
     _instruction = Instruction.BUY
-    # Can be changed to handle options
-    _instrument_asset_type = InstrumentAssetType.EQUITY
 
     _order_leg = create_equity_order_leg(
         instruction=_instruction, quantity=quantity, symbol=symbol,
@@ -103,4 +109,119 @@ def build_buy_market_stock_order(symbol, quantity):
         duration=duration,
         orderStrategyType=order_strategy_type,
         orderLegCollection=order_leg_collection,
+    )
+
+def build_buy_limit_option_order(symbol, quantity, price):
+    """Build Buy Limit: Single Option
+
+    Buy to open {quanity} contracts of the {symbol with date and option info}
+    at a Limit of {price} good for the Day.
+
+    Args:
+        symbol: symbol you want to trade.  Includes date e.g., XYZ_032015C49
+        quantity: Amount you want to buy
+        price: Amount you want to buy the option for
+
+    Note:
+        May want to add expiry date and type of option (CALL, PUT) and
+        create completed symbol
+    """
+    # Constants
+    complex_order_strategy_type = ComplexOrderStrategyType.NONE
+    order_type = OrderType.LIMIT
+    session = Session.NORMAL
+    duration = Duration.DAY
+    order_strategy_type = OrderStrategyType.SINGLE
+    _instruction = Instruction.BUY_TO_OPEN
+
+    _order_leg = create_option_order_leg(
+        instruction=_instruction, quantity=quantity, symbol=symbol,
+    )
+    order_leg_collection = [_order_leg]
+
+    return Order(
+        complexOrderStrategyType=complex_order_strategy_type,
+        orderType=order_type,
+        session=session,
+        price=price,
+        duration=duration,
+        orderStrategyType=order_strategy_type,
+        orderLegCollection=order_leg_collection,
+    )
+
+def build_buy_limit_vertical_call_spread_order(buy_symbol, sell_symbol, quantity, price):
+    """Buy Limit: Vertical Call Spread
+
+    Buy to open 10 contracts of the XYZ Jan 15, 2016 $40 Call and
+    Sell to open 10 contracts of the XYZ Jan 15, 2016 $42.5 Call
+    for a Net Debit of $1.20 good for the Day.
+
+    Args:
+        buy_symbol: symbol you want to buy.  Includes date e.g., XYZ_011516C40
+        sell_symbol: symbol you want to buy.  Includes date e.g., XYZ_011516C42.5
+        quantity: Amount you want to buy.
+        price: Amount you want to buy the spread for
+    """
+    # Constants
+    order_type = OrderType.NET_DEBIT
+    session = Session.NORMAL
+    duration = Duration.DAY
+    order_strategy_type = OrderStrategyType.SINGLE
+
+    _buy_order_leg = create_option_order_leg(
+        instruction=Instruction.BUY_TO_OPEN, quantity=quantity, symbol=buy_symbol,
+    )
+
+    _sell_order_leg = create_option_order_leg(
+        instruction=Instruction.SELL_TO_OPEN, quantity=quantity, symbol=sell_symbol,
+    )
+
+    order_leg_collection = [
+        _buy_order_leg,
+        _sell_order_leg,
+    ]
+
+    return Order(
+        orderType=order_type,
+        session=session,
+        price=price,
+        duration=duration,
+        orderStrategyType=order_strategy_type,
+        orderLegCollection=order_leg_collection,
+    )
+
+    
+def build_custom_option_spread_order(buy_symbol, sell_symbol, buy_quantity, sell_quantity):
+    """Custom Option Spread
+
+    Buy to open 2 contracts of the XYZ Jan 17, 2020 $43 Put and Sell to open
+    1 contracts of the XYZ Jan 18, 2019 $45 Put at the Market good for the Day.
+    """
+    # Constants
+    order_strategy_type = OrderStrategyType.SINGLE
+    order_type = OrderType.MARKET
+    session = Session.NORMAL
+    duration = Duration.DAY
+    complex_order_strategy_type = ComplexOrderStrategyType.CUSTOM
+
+    _buy_order_leg = create_option_order_leg(
+        instruction=Instruction.BUY_TO_OPEN, quantity=buy_quantity, symbol=buy_symbol,
+    )
+
+    _sell_order_leg = create_option_order_leg(
+        instruction=Instruction.SELL_TO_OPEN, quantity=sell_quantity, symbol=sell_symbol,
+    )
+
+    order_leg_collection = [
+        _sell_order_leg,
+        _buy_order_leg,
+    ]
+
+    return Order(
+        orderStrategyType=order_strategy_type,
+        orderType=order_type,
+        orderLegCollection=order_leg_collection,
+        complexOrderStrategyType=complex_order_strategy_type,
+        duration=duration,
+        session=session,
     )
