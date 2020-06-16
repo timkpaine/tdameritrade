4eae3bc1d5042d21f763fa6e9dee92bc429597e9
Jonathan Reyes <holostriker@gmail.com>
2020-06-16 01:17:14 -0400

    Update orders doc

--- tdameritrade/orders/__init__.py
+++ tdameritrade/orders/__init__.py
@@ -3,7 +3,7 @@
 This module is used to construct valid json for the place orders endpoint.
 https://developer.tdameritrade.com/account-access/apis/post/accounts/%7BaccountId%7D/orders-0
 
-The validation is done in the *_builder functions for now
+The validation is done by the orders.models.base.BaseOrder class
 
 The order_builder file incudes sample order builder functions created from
 https://developer.tdameritrade.com/content/place-order-samples
