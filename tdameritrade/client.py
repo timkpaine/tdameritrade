b35d301f4ff5653f84b54c292d4941506a7185bc
Wilfred Cho <wilfredcho@gmail.com>
2020-09-07 10:31:32 -0700

    fix order

--- tdameritrade/client.py
+++ tdameritrade/client.py
@@ -471,7 +471,7 @@ class TDClient(object):
             params['fromEnteredTime'] = fromEnteredTime
         if toEnteredTime:
             params['toEnteredTime'] = toEnteredTime
-        return self._request(GET_ORDER_BY_QUERY, params=params).json()
+        return self._request(GET_ORDER_BY_QUERY, json=params)
 
     def cancelOrder(self, accountId, orderId):
         '''cancel the given order
